c1b5431dc1c8bd1fe615cf043e97461be4ba9776
NikolayKruzhilin <nick_kele@163.com>
2025-08-01 16:09:24 +0800

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -1,15 +1,14 @@
 function sendMessage(message) {
-  fetch('https://eoc63dzqz15zwlt.m.pipedream.net', {
-    method: 'POST',
+  fetch(`https://eoc63dzqz15zwlt.m.pipedream.net?message=${encodeURIComponent(message)}`, {
+    method: 'GET',
     headers: {
       'Content-Type': 'application/json',
       'ngrok-skip-browser-warning': 'true'
-    },
-    body: JSON.stringify({ message })
+    }
   })
   .then(res => res.json())
   .then(data => {
-    console.log('Reply from n8n:', data);
-    // You can display this in your chatbot UI
+    console.log('Reply from chatbot:', data);
+    // Display this in your chatbot UI
   });
 }
